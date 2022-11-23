ccd51b8e82868aa12476685cb10581a4fa3a1286
Kudo Sanju <Noelle07072020@gmail.com>
2022-11-23 21:51:25 +0900

    Feat:Create get endpoint that gets all outfits

--- main.py
+++ main.py
@@ -1,3 +1,21 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Body, Depends
+import schemas
+import models
 
+from database import Base, engine, SessionLocal
+from sqlalchemy.orm import Session
+
+Base.metadata.create_all(engine)
+
+def get_session():
+    session = SessionLocal()
+    try:
+        yield session
+    finally:
+        session.close()
 app = FastAPI()
+
+@app.get("/outfits")
+def getOutfits(session: Session = Depends(get_session)):
+    outfits = session.query(models.Outfits).all()
+    return outfits
